e789b6395d4b8795fd71d5b3b653f42bba5f4999
h2pl <362294931@qq.com>
2023-12-13 11:16:16 +0800

    java

--- src/main/java/com/example/temporal/opentelemetry/test/OpenTelemetryCase.java
+++ src/main/java/com/example/temporal/opentelemetry/test/OpenTelemetryCase.java
@@ -1,6 +1,7 @@
 package com.example.temporal.opentelemetry.test;
 
 import com.alibaba.fastjson.JSON;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
@@ -57,11 +58,17 @@ public class OpenTelemetryCase {
     }
 
     public static void main(String[] args) {
+        demo();
+    }
+
+    public static void useCase() {
         System.out.println("initOpenTelemetry");
-        OpenTelemetry openTelemetry = TracingConfig.initOpenTelemetryProperties();
+        String name = System.getenv("OTEL_SERVICE_NAME");
+        System.out.println(name);
+
+        OpenTelemetry openTelemetry = TracingConfig.initOpenTelemetryManually();
         log.info("openTelemetry" + JSON.toJSONString(openTelemetry));
 
-//        OpenTelemetry openTelemetry = TracingConfig.initOpenTelemetry("http://localhost:4317");
         System.out.println("getTracer");
         Tracer tracer = openTelemetry.getTracer("io.opentelemetry.example.JaegerExample");
 
@@ -76,18 +83,41 @@ public class OpenTelemetryCase {
 
     }
 
-//    public static void testDynatraceAPI(){
-//        System.out.println("initOpenTelemetry");
-//        OpenTelemetry openTelemetry = Api.initOpenTelemetryProperties();
-////        OpenTelemetry openTelemetry = TracingConfig.initOpenTelemetry("http://localhost:4317");
-//        System.out.println("getTracer");
-//        Tracer tracer = openTelemetry.getTracer("io.opentelemetry.example.JaegerExample");
-//
-//        // it is important to initialize your SDK as early as possible in your application's lifecycle
-//        System.out.println("begin");
-//        // generate a few sample spans
-//        myWonderfulUseCase(tracer);
-//
-//        System.out.println("end");
-//    }
+    public static void demo() {
+        TracingConfig.initOpenTelemetryManually();
+
+        Tracer tracer = GlobalOpenTelemetry
+                .getTracerProvider()
+                .tracerBuilder("my-tracer") //TODO Replace with the name of your tracer
+                .build();
+
+        // Obtain and name new span from tracer
+        Span span = tracer.spanBuilder("Call to /myendpoint").startSpan();
+
+// Set demo span attributes using semantic naming
+        span.setAttribute("http.method", "GET");
+        span.setAttribute("net.protocol.version", "1.1");
+
+// Set the span as current span and parent for future child spans
+        try (Scope scope = span.makeCurrent())
+        {
+            int rd = ThreadLocalRandom.current().nextInt(1, 100);
+            span.addEvent("res="  + rd);
+            span.setAttribute("key","value");
+            if (rd < 50){
+                span.setStatus(StatusCode.ERROR);
+            }else {
+                span.setStatus(StatusCode.OK);
+            }
+
+            System.out.println(rd);
+            // TODO your code goes here
+        }
+        finally
+        {
+            // Completing the span
+            span.end();
+        }
+    }
+
 }
--- src/main/java/com/example/temporal/opentelemetry/test/TracingConfig.java
+++ src/main/java/com/example/temporal/opentelemetry/test/TracingConfig.java
@@ -1,23 +1,37 @@
 package com.example.temporal.opentelemetry.test;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter;
+import io.opentelemetry.exporter.otlp.http.metrics.OtlpHttpMetricExporter;
 import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.AggregationTemporalitySelector;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.semconv.ResourceAttributes;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class TracingConfig {
     public static OpenTelemetry initOpenTelemetry(String jaegerEndpoint) {
@@ -93,6 +107,108 @@ public class TracingConfig {
 
             return resource.merge(dtMetadata);
         }).build().getOpenTelemetrySdk();
+        OpenTelemetryAppender.install(sdk);
+
+        return sdk;
+    }
+
+    private static final String DT_API_URL = "https://aki10000.live.dynatrace.com/api/v2/otlp"; // TODO: Provide your SaaS/Managed URL here
+    private static final String DT_API_TOKEN = "dt0c01.BZCWAQJ6RQ6BYCNCIRD7WIKV.BCAYABR367UJDE3TJ6H7GB7QYGITGLVFQUCD4STGL6QJVDE4BVWOVYCMSMPDO4YI"; // TODO: Provide the OpenTelemetry-scoped access token here
+    public static OpenTelemetry initOpenTelemetryManually(){
+        // ===== GENERAL SETUP =====
+
+        // Read service name from the environment variable OTEL_SERVICE_NAME, if present
+        Resource serviceName = Optional.ofNullable(System.getenv("OTEL_SERVICE_NAME"))
+                .map(n -> Attributes.of(AttributeKey.stringKey("service.name"), n))
+                .map(Resource::create)
+                .orElseGet(Resource::empty);
+
+        // Parse the environment variable OTEL_RESOURCE_ATTRIBUTES into key-value pairs
+        Resource envResourceAttributes = Resource.create(Stream.of(Optional.ofNullable(System.getenv("OTEL_RESOURCE_ATTRIBUTES")).orElse("").split(","))
+                .filter(pair -> pair != null && pair.length() > 0 && pair.contains("="))
+                .map(pair -> pair.split("="))
+                .filter(pair -> pair.length == 2)
+                .collect(Attributes::builder, (b, p) -> b.put(p[0], p[1]), (b1, b2) -> b1.putAll(b2.build()))
+                .build()
+        );
+
+        // Read host information from OneAgent files to enrich telemetry
+        Resource dtMetadata = Resource.empty();
+        for (String name : new String[] {"dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties", "/var/lib/dynatrace/enrichment/dt_metadata.properties", "/var/lib/dynatrace/enrichment/dt_host_metadata.properties"}) {
+            try {
+                Properties props = new Properties();
+                props.load(name.startsWith("/var") ? new FileInputStream(name) : new FileInputStream(Files.readAllLines(Paths.get(name)).get(0)));
+
+
+                dtMetadata = dtMetadata.merge(Resource.create(
+                        props.entrySet().stream()
+                                .collect(Attributes::builder, (b, e) -> b.put(e.getKey().toString(), e.getValue().toString()), (b1, b2) -> b1.putAll(b2.build()))
+                                .build()
+                ));
+            } catch (IOException e) {}
+        }
+
+        // ===== TRACING SETUP =====
+
+        // Configure span exporter with the Dynatrace URL and the API token
+        SpanExporter exporter = OtlpHttpSpanExporter.builder()
+                .setEndpoint(DT_API_URL + "/v1/traces")
+                .addHeader("Authorization", "Api-Token " + DT_API_TOKEN)
+                .build();
+
+
+        // Set up tracer provider with a batch processor and the span exporter
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .setResource(Resource.getDefault().merge(envResourceAttributes).merge(serviceName).merge(dtMetadata))
+                .setSampler(Sampler.alwaysOn())
+                .addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
+                .build();
+
+
+        // ===== METRIC SETUP =====
+
+
+        // Configure metric exporter with the Dynatrace URL and the API token
+        OtlpHttpMetricExporter metricExporter = OtlpHttpMetricExporter.builder()
+                .setEndpoint(DT_API_URL + "/v1/metrics")
+                .addHeader("Authorization", "Api-Token " + DT_API_TOKEN)
+                .setAggregationTemporalitySelector(AggregationTemporalitySelector.deltaPreferred())
+                .build();
+
+
+        // Set up meter provider with a periodic reader and the metric exporter
+        SdkMeterProvider meterProvider = SdkMeterProvider.builder()
+                .setResource(Resource.getDefault().merge(envResourceAttributes).merge(serviceName).merge(dtMetadata))
+                .registerMetricReader(PeriodicMetricReader.builder(metricExporter).build())
+                .build();
+
+        // ===== LOG SETUP =====
+
+        // Configure log exporter with the Dynatrace URL and the API token
+        OtlpHttpLogRecordExporter logExporter = OtlpHttpLogRecordExporter.builder()
+                .setEndpoint(DT_API_URL + "/v1/logs")
+                .addHeader("Authorization", "Api-Token " + DT_API_TOKEN)
+                .build();
+
+
+        // Set up log provider with the log exporter
+        SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
+                .setResource(Resource.getDefault().merge(envResourceAttributes).merge(serviceName).merge(dtMetadata))
+                .addLogRecordProcessor(BatchLogRecordProcessor.builder(logExporter).build())
+                .build();
+
+        // ===== INITIALIZATION =====
+
+        // Initialize OpenTelemetry with the tracer and meter providers
+        OpenTelemetrySdk sdk = OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .setMeterProvider(meterProvider)
+                .setLoggerProvider(sdkLoggerProvider)
+                .buildAndRegisterGlobal();
+
+        //
+        Runtime.getRuntime().addShutdownHook(new Thread(sdkTracerProvider::close));
         OpenTelemetryAppender.install(sdk);
 
         return sdk;
